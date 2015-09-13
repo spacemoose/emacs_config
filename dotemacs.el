e95a6abfde8accb55aabe9c5b18cc2675e6ee478
Glen Stark <@laptop>
2015-09-13 18:51:02 +0200

    stuff

--- dotemacs.el
+++ dotemacs.el
@@ -14,23 +14,13 @@
 (load "refactoring.el")
 (load "xentis.el")
 
-;(require 'semantic)
-;(require 'semantic/bovine/gcc)
-;; (add-to-list 'semantic-default-submodes 'global-semanticdb-minor-mode)
-;; (add-to-list 'semantic-default-submodes 'global-semantic-idle-local-symbol-highlight-mode)
-;; (add-to-list 'semantic-default-submodes 'global-semantic-idle-scheduler-mode)
-;; (add-to-list 'semantic-default-submodes 'global-semantic-idle-summary-mode)
-;; ;
-(semantic-mode 1)
-;(global-ede-mode t)
-;(ede-enable-generic-projects)
-
 
 (define-fringe-bitmap 'custom-hollow-right-arrow [128 192 96 48 24 48 96 192 128] 15 8 'center)
 
 (put 'overlay-arrow-position 'overlay-arrow-bitmap 'custom-hollow-right-arrow)
 
 (provide 'dotemacs)
+
 ;;; dotemacs.el ends here
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
